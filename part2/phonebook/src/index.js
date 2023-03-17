eb990f8a2e5ef0b689d6ebc0f2b1ba574b0da218
sam <manor@hey.com>
2023-03-17 15:46:11 -0400

    cleanup

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,11 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-// axios.get('http://localhost:3001/persons')
-//   .then(response => {
-//     const persons = response.data;
-//     console.log(persons);
-//   }
-// );
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
