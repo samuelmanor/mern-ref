c1e822e0c32ad8ade2cd669d3121ccd930710a66
sam <manor@hey.com>
2023-03-27 18:42:55 -0400

    completed 3.7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
+morgan.token('body', function (request) { return JSON.stringify(request.body) });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
