72751e7b88a4e4cc2c38d8b844e268bd4095ccd7
sam <manor@hey.com>
2023-03-28 18:14:52 -0400

    completed 3.13 and 3.14

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,41 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const Person = require('./models/Person');
 
 const app = express();
 
 app.use(express.json());
-morgan.token('body', function (request) { return JSON.stringify(request.body) });
+morgan.token('body', function (req) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-np123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then(persons => {
+        res.json(persons);
+    });
 });
 
 app.get('/info', (req, res) => {
@@ -48,14 +32,22 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(p => p.id === id);
+    Person.findById(req.params.id).then(person => {
+        res.json(person);
+    });
+});
 
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end();
-    };
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    });
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson);
+    });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -71,30 +63,6 @@ const genId = () => {
         : 0;
     return maxId + 1;
 };
-
-app.post('/api/persons', (req, res) => {
-    const body = req.body;
-
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'content missing'
-        });
-    } else if (persons.find(p => p.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        });
-    };
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: genId()
-    };
-    
-    persons = persons.concat(person);
-
-    res.json(person);
-});
 
 const PORT = 3001;
 app.listen(PORT, () => {
