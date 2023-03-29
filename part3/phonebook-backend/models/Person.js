dd4e83dd2a39302bdc3f2c7fd46e9294c28095e4
sam <manor@hey.com>
2023-03-28 20:46:11 -0400

    completed 3.19

--- part3/phonebook-backend/models/Person.js
+++ part3/phonebook-backend/models/Person.js
@@ -15,8 +15,15 @@ mongoose.connect(url)
     });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true
+    }
 });
 
 personSchema.set('toJSON', {
