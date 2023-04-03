2551bc69b3eb7776cd80abd2f9add342d867ccbc
sam <manor@hey.com>
2023-04-03 12:51:12 -0400

    completed 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
+const dummy = () => {
     return 1;
 };
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, current) => sum + current.likes, 0);
+};
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 };
