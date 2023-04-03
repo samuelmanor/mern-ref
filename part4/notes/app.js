66525b8c8026c0ab38f85b4424d70ab0d21aaa1c
sam <manor@hey.com>
2023-04-03 09:49:14 -0400

    finished 4a notes

--- part4/notes/app.js
+++ part4/notes/app.js
@@ -3,13 +3,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
 
+logger.info('connecting to', config.MONGODB_URI);
 
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB');
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message);
+    });
 
+app.ue(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
+app.use('/api/notes', notesRouter);
 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-
-
-// app.use('/api/notes', notesRouter);
+module.exports = app;
--- part4/notes/index.js
+++ part4/notes/index.js
@@ -1,4 +1,4 @@
-const app = require('./app'); // actual express application
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
