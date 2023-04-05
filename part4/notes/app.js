b8b2434dd3e4935d04e5c1e542fa4554d3045f89
sam <manor@hey.com>
2023-04-04 22:04:45 -0400

    refactored tests

--- part4/notes/app.js
+++ part4/notes/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
@@ -19,7 +20,6 @@ mongoose.connect(config.MONGODB_URI)
 	});
 
 app.use(cors());
-app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
