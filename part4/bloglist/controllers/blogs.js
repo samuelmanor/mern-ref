8c2b65442a06cdb15e1c5b8644d7ee7b8902ea24
sam <manor@hey.com>
2023-04-05 21:12:36 -0400

    completed 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
     response.json(blogs);
 });
 
@@ -16,10 +17,22 @@ blogsRouter.get('/:id', async (request, response, next) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body);
+    const body = request.body;
+
+    const user = await User.findById(body.userId);
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        likes: body.likes,
+        user: user.id
+    });
 
     const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
