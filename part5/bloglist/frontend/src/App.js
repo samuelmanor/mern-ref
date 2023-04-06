5b21283ef9c0207716dd6ebd17c98e26fc4295ea
sam <manor@hey.com>
2023-04-06 17:20:10 -0400

    completed 5.1 and 5.2

--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Blog from './components/blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => {setBlogs(blogs)});
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, [blogs]);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user));
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  };
+
+  const loginForm = (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  );
+
+  const blogMap = blogs.map(blog => <Blog key={blog.id} blog={blog} />);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user === null
+        ? loginForm
+        : <div><p>{user.name} logged in</p></div>}
+
+      <h2>blogs</h2>
+      {user === null ? null : blogMap}
     </div>
   );
-}
+};
 
 export default App;
