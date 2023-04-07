762abd6968182c32b3e5a9cfb4cc6b79638e97a9
sam <manor@hey.com>
2023-04-07 11:54:19 -0400

    completed 5.7 and 5.8

--- part5/bloglist/frontend/src/components/blog.js
+++ part5/bloglist/frontend/src/components/blog.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
+    const [showInfo, setShowInfo] = useState(false);
+
+    const toggleShow = () => {
+        setShowInfo(!showInfo)
+    }
+
     return (
         <div>
             {blog.title} - {blog.author}
+            <button onClick={toggleShow}>view</button>
+            <div style={{ display: showInfo ? '' : 'none' }}>
+                <p>{blog.url}</p>
+                <p>likes: {blog.likes}</p> <button>like</button>
+                <p>posted by {blog.user.username}</p>
+            </div>
         </div>
     );
 };
