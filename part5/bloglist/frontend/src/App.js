9614a8753a8eae4ac516c47e1f8f96ac2f67488a
sam <manor@hey.com>
2023-04-07 12:31:27 -0400

    completed 5.10

--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -73,7 +73,12 @@ const App = () => {
     }, 5000);
   };
 
-  const blogMap = blogs.map(blog => <Blog key={blog.id} blog={blog} handleUpdate={handleUpdate} />);
+  const sortedBlogs = () => {
+    const copy = blogs.concat();
+    return copy.sort((a, b) => b.likes - a.likes);
+  };
+
+  const blogMap = sortedBlogs().map(blog => <Blog key={blog.id} blog={blog} handleUpdate={handleUpdate} />);
 
   return (
     <div>
