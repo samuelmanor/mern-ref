1748864936ca85be1d9dd24958d8d79eaa737d37
sam <manor@hey.com>
2023-04-08 23:52:47 -0400

    completed 5.20

--- part5/bloglist/frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist/frontend/cypress/e2e/bloglist.cy.js
@@ -1,13 +1,21 @@
 describe('Bloglist', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`);
+
     const user = {
       name: 'Samuel Manor',
       username: 'smm',
       password: 'password'
     };
+    const testUser = {
+      name: 'Superuser',
+      username: 'root',
+      password: 'toor'
+    };
 
-    cy.request('POST', 'http://localhost:3001/api/users', user);
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user);
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, testUser);
+
     cy.visit('http://localhost:3000');
   });
 
@@ -22,9 +30,7 @@ describe('Bloglist', function() {
 
   describe('login', function() {
     it('user can log in', function() {
-      cy.get('#username').type('smm');
-      cy.get('#password').type('password');
-      cy.get('#login-btn').click();
+      cy.login({ username: 'smm', password: 'password' });
   
       cy.contains('Samuel Manor logged in');
     });
@@ -41,17 +47,43 @@ describe('Bloglist', function() {
 
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.get('#username').type('smm');
-      cy.get('#password').type('password');
-      cy.get('#login-btn').click();
+      cy.login({ username: 'smm', password: 'password' });
+
+      cy.createBlog({
+        title: 'Out There: On Not Finishing',
+        author: 'Devin Kelly',
+        url: 'https://longreads.com/2020/09/08/out-there-on-not-finishing/'
+      });
+
+      cy.createBlog({
+        title: 'Make Peace With Your Unlived Life',
+        author: 'Manfred F. R. Kets de Vries',
+        url: 'https://hbr.org/2016/12/make-peace-with-your-unlived-life'
+      });
+
+      cy.createBlog({
+        title: 'Death to Minimalism',
+        author: 'Nathan J. Robinson',
+        url: 'https://www.currentaffairs.org/2019/02/death-to-minimalism'
+      })
     });
 
-    it.only('a new note can be created', function() {
+    it('a new blog can be created', function() {
       cy.contains('new blog').click();
-      cy.get('#title-input').type('lorem ipsum');
-      cy.get('#author-input').type('john smith');
-      cy.get('#url-input').type('www.website.com');
+      cy.get('#title-input').type('Neanderthals With Disabilities Survived Through Social Support');
+      cy.get('#author-input').type('George Dvorsky');
+      cy.get('#url-input').type('https://www.gizmodo.com.au/2017/10/neanderthals-with-disabilities-survived-through-social-support/');
       cy.get('#post-blog-btn').click();
+
+      cy.contains('- George Dvorsky');
     });
+
+    it.only('a blog can be liked', function() {
+      cy.contains('view').click();
+      cy.get('#like-btn').click().click();
+
+      cy.contains('likes: 1');
+    })
+
   });
 });
