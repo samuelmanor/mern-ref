76b4369e3fc255e9f6b17745d94d2816b78df851
sam <manor@hey.com>
2023-04-08 23:17:16 -0400

    completed 5.19

--- part5/bloglist/frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist/frontend/cypress/e2e/bloglist.cy.js
@@ -20,20 +20,38 @@ describe('Bloglist', function() {
     cy.contains('password');
   });
 
-  it('user can log in', function() {
-    cy.get('#username').type('smm');
-    cy.get('#password').type('password');
-    cy.get('#login-btn').click();
-
-    cy.contains('Samuel Manor logged in');
+  describe('login', function() {
+    it('user can log in', function() {
+      cy.get('#username').type('smm');
+      cy.get('#password').type('password');
+      cy.get('#login-btn').click();
+  
+      cy.contains('Samuel Manor logged in');
+    });
+  
+    it('user fails with wrong password', function() {
+      cy.get('#username').type('smm');
+      cy.get('#password').type('hello');
+      cy.get('#login-btn').click();
+  
+      cy.contains('wrong username or password');
+      cy.get('html').should('not.contain', 'Samuel Manor logged in');
+    });
   });
 
-  it('user fails with wrong password', function() {
-    cy.get('#username').type('smm');
-    cy.get('#password').type('hello');
-    cy.get('#login-btn').click();
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.get('#username').type('smm');
+      cy.get('#password').type('password');
+      cy.get('#login-btn').click();
+    });
 
-    cy.contains('wrong username or password');
-    cy.get('html').should('not.contain', 'Samuel Manor logged in');
+    it.only('a new note can be created', function() {
+      cy.contains('new blog').click();
+      cy.get('#title-input').type('lorem ipsum');
+      cy.get('#author-input').type('john smith');
+      cy.get('#url-input').type('www.website.com');
+      cy.get('#post-blog-btn').click();
+    });
   });
 });
