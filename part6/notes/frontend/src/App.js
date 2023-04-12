59b48f8541031215cd58c3f2d474de2d8e232e0c
sam <manor@hey.com>
2023-04-12 17:34:28 -0400

    completed 6a notes

--- part6/notes/frontend/src/App.js
+++ part6/notes/frontend/src/App.js
@@ -1,38 +1,11 @@
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
-
-const generateId = () => {
-  Number((Math.random() * 1000000).toFixed(0));
-};
+import NewNote from "./components/newnote"
+import Notes from "./components/notes"
 
 const App = () => {
-  const dispatch = useDispatch();
-  const notes = useSelector(state => state);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = ''
-    store.dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id));
-  };
-
-  return(
+  return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" /> 
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map(note=>
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
