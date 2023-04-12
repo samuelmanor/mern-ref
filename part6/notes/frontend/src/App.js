af3f6a2e70160ff45b2476a09e869994f3acdab0
sam <manor@hey.com>
2023-04-11 22:57:16 -0400

    still working

--- part6/notes/frontend/src/App.js
+++ part6/notes/frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const generateId = () => {
+  Number((Math.random() * 1000000).toFixed(0));
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector(state => state);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = ''
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id));
+  };
+
+  return(
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map(note=>
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
     </div>
   );
-}
+};
 
 export default App;
