073f5327a3ae20e854a15d0cbed0ddee6ed00065
sam <manor@hey.com>
2023-04-13 11:02:49 -0400

    completed 6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { setNotif } from '../reducers/notificationReducer';
+import anecdoteService from '../services/anecdotes';
 
 const NewAnecdote = () => {
     const dispatch = useDispatch();
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault();
         const content = event.target.anecdote.value;
         event.target.anecdote.value = '';
-        dispatch(createAnecdote(content));
 
-        dispatch(setNotif('created xyz'));
+        const newAnecdote = await anecdoteService.createNew(content);
+        dispatch(createAnecdote(newAnecdote));
+
+        dispatch(setNotif(`created ${content}`));
         setTimeout(() => {
             dispatch(setNotif(''));
         }, 5000);
