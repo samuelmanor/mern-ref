76f67efa9d1b345205a843f5a109a948e404362e
sam <manor@hey.com>
2023-04-13 11:38:11 -0400

    completed 6.16

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,15 +5,14 @@ import Notification from './components/Notification';
 
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)));
+    dispatch(initializeAnecdotes());
   }, [dispatch]);
-  
+
   return (
     <div>
       <Notification />
