d16d7636f54adc9c39fbe63231033a2c1b57c153
sam <manor@hey.com>
2023-04-15 10:34:02 -0400

    completed 8.5

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,11 +112,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books;
+      } else if (args.author && !args.genre) {
+        return books.filter(b => b.author === args.author);
+      } else if (!args.author && args.genre) {
+        return books.filter(b => b.genre === args.genre);
+      } else if (args.author && args.genre) {
+        return books.filter(b => b.author === args.author && (b.genres).includes(args.genre));
       }
-      const byAuthor = books.filter(b => b.author === args.author);
-      return byAuthor;
     },
     allAuthors: () => authors
   },
